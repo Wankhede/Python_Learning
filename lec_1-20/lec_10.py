b222740ab0ebea946c4a26780e4c18c2b90a2335
Wankhede <spwankhede007@gmail.com>
2025-04-17 23:35:53 +0530

    Adding lec 10 n 11

---
+++ lec_1-20/lec_10.py
@@ -0,0 +1,46 @@
+#Shift Operators
+
+a = 5
+res = a << 0
+print(res)
+res = a << 1  # This is simply multiply a with 2 raise to the shift operator
+print(res)
+res = a << 2
+print(res)
+res = a << 3
+print(res)
+res = a << 4
+print(res)
+res = a << 5
+print(res)
+res = a << 6
+print(res)
+print("###########################################")
+b = res >> 0 # This is simply num divide by 2 raise to left shift operator
+print(b)
+b = res >> 1
+print(b)
+b = res >> 2
+print(b)
+b = res >> 3
+print(b)
+b = res >> 4
+print(b)
+b = res >> 5
+print(b)
+b = res >> 6
+print(b)
+b = res >> 7
+print(b)
+b = res >> 8
+print(b)
+b = res >> 9
+print(b)
+b = res >> 10
+print(b)
+del b
+
+s1 = "512"
+print(type(s1))
+s2 = int(s1)
+print(type(s2))
