b689ed9ff08089674171a2c7f83274ac195bd2dd
Wankhede <spwankhede007@gmail.com>
2025-04-19 00:19:43 +0530

    Adding few more

---
+++ lec_1-20/lec_14.py
@@ -0,0 +1,20 @@
+#Checking the if , elif and else condition block in python
+
+n1 = 0
+n2 = 2
+
+if n1 > n2:
+    print("n1 > n2")
+elif n2 > n1:
+    print("n2 > n1")
+elif n1 == n2 == 0:
+    print("Zero")
+else:
+    print("Equal")
+
+#Checking the execution of the while statement in python
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
---
+++ lec_1-20/lec_15.py
@@ -0,0 +1,25 @@
+#While statement usecase: -
+# To be able to repeat an execution of the statement of a block of statement without having to write them repeatedly
+
+
+# We want to write "Hello" 5 times
+
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+a = 5
+b = 10
+# while a < b:
+#     print("Hello World !!!")
+
+s = input("Enter an input ")
+ss = int(s)
+print(a,ss)
+while a > ss:               # while this is true
+    print("Hello Swapnil")  # Execute this block
+    ss = ss+1
+    print(a)
+
