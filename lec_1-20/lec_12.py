008be681d599531ae04ab163760bd6ab29245f04
Wankhede <spwankhede007@gmail.com>
2025-04-18 23:25:14 +0530

    Adding lec 12 and 13

---
+++ lec_1-20/lec_12.py
@@ -0,0 +1,5 @@
+#Control flow of application
+# 1) The source code of the python is made up of multiple statements
+# 2) Sequence in which the statement in the source code are executed while running the application
+#    is known as the CONTROL FLOW OF THE APPLICATION.
+
---
+++ lec_1-20/lec_13.py
@@ -0,0 +1,24 @@
+print("Start of Program")
+
+n1 = int(input("Enter an Integer"))
+
+n2 = int(input("Enter another Integer"))
+
+if n1 > n2:
+    print(n1, " is greater than " , n2)
+else:
+    print(n2 , " is greater than " , n1)
+
+print("End of Program")
+
+#If statement is a block of statement written in python which allows programmer to execute the statements
+#conditionally
+
+#
+print("Hello Python World!!!")
+n1 = 20
+n2 = 30
+if n1 >  n2:
+    print("n1 is greater")
+else:
+    print("n2 is greater")
